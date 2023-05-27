a00583a294a877a8a3054053ac313a94164963f2
Alexey Lazarev <alazarev@vivid.money>
2023-05-27 13:50:43 +0600

    add tests

---
+++ tests/values_test.go
@@ -0,0 +1,25 @@
+package tests
+
+import (
+	"testing"
+
+	"main/render"
+)
+
+func TestRunRender(t *testing.T) {
+
+	t.Run("template-values.yaml", func(t *testing.T) {
+		valuesRender := new(render.ValuesRenderer)
+		valuesRender.Run("template-values.yaml", "")
+	})
+
+	t.Run("base1-values1.yaml", func(t *testing.T) {
+		valuesRender := new(render.ValuesRenderer)
+		valuesRender.Run("base-values1.yaml", "")
+	})
+
+	// t.Run("extended-values1.yaml", func(t *testing.T) {
+	// 	valuesRender := new(render.ValuesRenderer)
+	// 	valuesRender.Run("extended-values1.yaml", "")
+	// })
+}
