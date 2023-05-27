aae32048cbcdf139fa8b9685d9059fdbf8b8d097
Alexey Lazarev <alazarev@vivid.money>
2023-05-27 12:40:40 +0600

    refactoring

--- main.go
+++ main.go
@@ -2,15 +2,10 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
-	"strings"
-	"text/template"
-
-	"gopkg.in/yaml.v2"
 )
 
 var (
@@ -22,37 +17,25 @@ const (
 	argPrefix = "render://"
 )
 
-type Values map[interface{}]interface{}
-
-type FilesList struct {
-	ImportValues []string `yaml:"importValuesFrom"`
-	ExtendRender []string `yaml:"extendRenderWith"`
-}
-
-type ValuesRenderer struct {
-	filename string
-	files    FilesList
-	values   Values
-}
-
 func main() {
 	flag.Parse()
+	args := flag.Args()
 	errLog.SetOutput(os.Stderr)
-
-	chartArg := os.Args[len(os.Args)-1]
-	vlRender := new(ValuesRenderer)
-	vlRender.filename = strings.TrimPrefix(chartArg, argPrefix)
-
-	vlRender.GetFileList()
-	vlRender.ReadValues()
-	vlRender.RenderTemplate()
+	if len(args) == 0 {
+		errLog.Fatalf(`
+		ERROR: Required arguments are missing.
+		Usage: %s %s<values-filename.yaml>`, filepath.Base(os.Args[0]), argPrefix)
+	}
+	argFilename := args[len(args)-1]
+	valuesRender := new(ValuesRenderer)
+	valuesRender.Run(argFilename, argPrefix)
 }
 
-// Read file from disk
+// Read the file.
 func readFile(file string) []byte {
 	data, err := ioutil.ReadFile(file)
 	if err != nil {
-		errLog.Fatalf("Error: \"%v\"}", err)
+		errLog.Fatalf("ERROR: \"%v\"}", err)
 	}
 	return data
 }
@@ -73,139 +56,19 @@ func mergeKeys(left, right Values) Values {
 	return left
 }
 
-// Get file list from importValuesFrom/extendRenderWith
-func (vr *ValuesRenderer) GetFileList() error {
-	dir := filepath.Dir(vr.filename)
-	rawFile := readFile(vr.filename)
-	yamlFiles := strings.ReplaceAll(string(rawFile), "{{", "#{{")
-	data := FilesList{}
-	err := yaml.Unmarshal([]byte(yamlFiles), &data)
-	if err != nil {
-		errLog.Fatalf("Error GetValuesFiles: Can't parse file: \"%s\"; stack:\"%v\"", vr.filename, err)
-	}
-	if len(data.ImportValues) < 1 {
-		println("Info: there is no import values.")
-	} else {
-		for i, source := range data.ImportValues {
-			if source == "self" {
-				data.ImportValues[i] = vr.filename
-				println("Info: there is itself using for values.")
-			} else {
-				data.ImportValues[i] = filepath.Join(dir, source)
-			}
-		}
-	}
-	if len(data.ExtendRender) < 1 {
-		println("Info: there is no extended files for render.")
-	} else {
-		for i, source := range data.ExtendRender {
-			data.ExtendRender[i] = filepath.Join(dir, source)
-		}
-	}
-
-	data.ExtendRender = append(data.ExtendRender, vr.filename)
-	vr.files = data
-
-	return err
-}
-
-func Dir2map(dir []string, val Values) Values {
+// If glob is used. Then map folder tree as yaml structure.
+func DirrectoryMapping(dir []string, val Values) Values {
 	data := make(Values)
 	if len(dir) > 1 {
-		data[dir[0]] = Dir2map(dir[1:], val)
+		data[dir[0]] = DirrectoryMapping(dir[1:], val)
 	} else {
 		data[dir[0]] = val
 	}
 	return data
 }
 
-func ReadMatch(pattern string) Values {
-	data := make(Values)
-	matches, _ := filepath.Glob(pattern)
-
-	for _, file := range matches {
-
-		dir := strings.Split(filepath.Dir(file), "/")
-		fileValue := make(Values)
-
-		rawFile := readFile(file)
-		err := yaml.Unmarshal(rawFile, &fileValue)
-		if err != nil {
-			errLog.Fatalf("Error ReadValues: Can't parse file: \"%s\"; stack:\"%v\"", file, err)
-		}
-		mergeKeys(data, Dir2map(dir, fileValue))
-	}
-	return data
-}
-
-// Read values from files
-func (vr *ValuesRenderer) ReadValues() {
-
-	vals := make(Values)
-
-	for _, file := range vr.files.ImportValues {
-		var rawFile []byte
-		var data Values
-
-		if strings.Contains(file, "*") {
-			println("Read files with mask")
-			data = ReadMatch(file)
-		} else {
-			rawFile = readFile(file)
-
-			yamlFiles := strings.ReplaceAll(string(rawFile), "{{", "#{{")
-			err := yaml.Unmarshal([]byte(yamlFiles), &data)
-			if err != nil {
-				errLog.Fatalf("Error ReadValues: Can't parse file: \"%s\"; stack:\"%v\"", file, err)
-			}
-		}
-		mergeKeys(vals, data)
-	}
-	if len(vals) == 0 {
-		vals = Values{}
-	}
-	vr.values = make(Values)
-
-	// errLog.Fatalf("RESULT %v\n", vals)
-
-	vr.values["Values"] = vals
-}
-
-// Render template to stdout
-func (vr *ValuesRenderer) RenderTemplate() {
-	valuesResult := make(Values)
-	for _, file := range vr.files.ExtendRender {
-		tpl, err := template.New("render").Funcs(funcMap()).ParseFiles(file)
-
-		if err != nil {
-			errLog.Fatalf("Error create render: %v}", err)
-		}
-		tpl.Option("missingkey=error")
-		var buf strings.Builder
-
-		err = tpl.ExecuteTemplate(&buf, filepath.Base(file), vr.values)
-		if err != nil {
-			errLog.Fatalf("Error: Can't render template: %v }", err)
-		}
-		rendered := strings.ReplaceAll(buf.String(), "<no value>", "")
-
-		// merge output in a single YAML
-		data := make(Values)
-		err = yaml.Unmarshal([]byte(rendered), &data)
-		if err != nil {
-			errLog.Fatalf("Error ReadValues: Can't parse file: \"%s\"; stack:\"%v\"", file, err)
-		}
-		mergeKeys(valuesResult, data)
-
-	}
-	renderedValues, err := yaml.Marshal(valuesResult)
-	if err != nil {
-		log.Fatalf("Error: Can't  marshal YAML :\"%v\"\n  \"%s\"", err, valuesResult)
-	}
-	if *debugMode {
-		println("Debug: rendered ##\n", string(renderedValues), "###\n")
-	}
-
-	fmt.Println(string(renderedValues))
-
+// Make file path absolut depend on main file.
+func absolutePath(basefile, depenfile string) string {
+	dir := filepath.Dir(basefile)
+	return filepath.Join(dir, depenfile)
 }
---
+++ values.go
@@ -0,0 +1,184 @@
+package main
+
+import (
+	"fmt"
+	"path/filepath"
+	"strings"
+	"text/template"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Values map[interface{}]interface{}
+
+type Files struct {
+	ImportValues []string `yaml:"importValuesFrom"`
+	ExtendRender []string `yaml:"extendRenderWith"`
+}
+
+type ValuesRenderer struct {
+	filename string
+	files    Files
+	values   Values
+}
+
+// Create new render.
+func (vr *ValuesRenderer) Run(filename, argPrefix string) {
+	if *debugMode {
+		errLog.Printf("DEBUG: filename: %s", string(filename))
+	}
+	vr.filename = strings.TrimPrefix(filename, argPrefix)
+
+	if err := vr.GetFiles(); err != nil {
+		errLog.Fatalf("ERROR: %s", err)
+	}
+	if err := vr.ReadValues(); err != nil {
+		errLog.Fatalf("ERROR:  %s", err)
+	}
+	if err := vr.RenderTemplate(); err != nil {
+		errLog.Fatalf("ERROR:  %s", err)
+	}
+}
+
+// Get files for importValuesFrom/extendRenderWith.
+func (vr *ValuesRenderer) GetFiles() error {
+
+	rawFile := readFile(vr.filename)
+	yamlFile := strings.ReplaceAll(string(rawFile), "{{", "#{{")
+	extraFiles := Files{}
+	dmsgs := []string{}
+	err := yaml.Unmarshal([]byte(yamlFile), &extraFiles)
+	if err != nil {
+		return fmt.Errorf("broken file: \"%s\" reason: \"%v\"", vr.filename, err)
+	}
+	if len(extraFiles.ImportValues) > 0 {
+		for i, importFilename := range extraFiles.ImportValues {
+			if importFilename == "self" {
+				extraFiles.ImportValues[i] = vr.filename
+				dmsgs = append(dmsgs, "importValuesFiles: self file will been used for values\n")
+			} else {
+				extraFiles.ImportValues[i] = absolutePath(vr.filename, importFilename)
+				dmsgs = append(dmsgs, "importValuesFiles: extrafile "+importFilename+" will been used for values\n")
+			}
+		}
+	} else {
+		extraFiles.ImportValues = append(extraFiles.ImportValues, vr.filename)
+		dmsgs = append(dmsgs, "importValuesFiles: there is no import values. Selfile will been used\n")
+	}
+	if len(extraFiles.ExtendRender) > 0 {
+		for i, renderFilername := range extraFiles.ExtendRender {
+			extraFiles.ExtendRender[i] = absolutePath(vr.filename, renderFilername)
+			dmsgs = append(dmsgs, "extendRenderWith: extrafile "+renderFilername+" will been rendred\n")
+		}
+	} else {
+		dmsgs = append(dmsgs, "extendRenderWith: there is no extended files for render\n")
+	}
+
+	extraFiles.ExtendRender = append(extraFiles.ExtendRender, vr.filename)
+	vr.files = extraFiles
+
+	if *debugMode {
+		errLog.Printf("DEBUG: %v\n", dmsgs)
+	}
+	return err
+}
+
+// Read values from files.
+func (vr *ValuesRenderer) ReadValues() error {
+
+	vals := make(Values)
+
+	for _, file := range vr.files.ImportValues {
+		var rawFile []byte
+		var data Values
+
+		if strings.Contains(file, "*") {
+			println("Read files with mask")
+			data = ParseYamlGlogFile(file)
+		} else {
+			rawFile = readFile(file)
+			if file == vr.filename {
+				yamlFile := strings.ReplaceAll(string(rawFile), "{{", "#{{")
+				rawFile = []byte(yamlFile)
+			}
+			err := yaml.Unmarshal(rawFile, &data)
+			if err != nil {
+				return fmt.Errorf("broken file: \"%s\" reason: \"%v\"", file, err)
+			}
+		}
+		mergeKeys(vals, data)
+	}
+	if len(vals) == 0 {
+		vals = Values{}
+	}
+	vr.values = make(Values)
+
+	if *debugMode {
+		errLog.Printf("DEBUG: total values: %#v\n", vals)
+	}
+	vr.values["Values"] = vals
+	return nil
+}
+
+// Read glob files.
+func ParseYamlGlogFile(pattern string) Values {
+
+	var data Values
+	matches, err := filepath.Glob(pattern)
+	if err != nil {
+		errLog.Fatalf("ERROR: wrong glob: \"%s\"; stack:\"%v\"", pattern, err)
+	}
+	for _, file := range matches {
+
+		dir := strings.Split(filepath.Dir(file), "/")
+		fileValue := make(Values)
+
+		rawFile := readFile(file)
+		err := yaml.Unmarshal(rawFile, &fileValue)
+		if err != nil {
+			errLog.Fatalf("ERROR: ReadValues: Can't parse file: \"%s\"; stack:\"%v\"", file, err)
+		}
+		mergeKeys(data, DirrectoryMapping(dir, fileValue))
+	}
+	return data
+}
+
+// Render Values template to a stdout.
+func (vr *ValuesRenderer) RenderTemplate() error {
+
+	var valuesResult Values
+	for _, file := range vr.files.ExtendRender {
+		var data Values
+		var buf strings.Builder
+		tpl, err := template.New("render").Funcs(funcMap()).ParseFiles(file)
+
+		if err != nil {
+			return fmt.Errorf("can't create template: %s}", err)
+		}
+		tpl.Option("missingkey=error")
+
+		err = tpl.ExecuteTemplate(&buf, filepath.Base(file), vr.values)
+		if err != nil {
+			return fmt.Errorf("can't render template: %s}", err)
+		}
+		rendered := strings.ReplaceAll(buf.String(), "<no value>", "")
+
+		// merge output in a single YAML
+		err = yaml.Unmarshal([]byte(rendered), &data)
+		if err != nil {
+			return fmt.Errorf("can't  parse file: \"%s\"; stack:\"%s\"", file, err)
+		}
+		mergeKeys(valuesResult, data)
+	}
+	renderedValues, err := yaml.Marshal(valuesResult)
+	if *debugMode {
+		errLog.Printf("DEBUG: rendered: %#v", string(renderedValues))
+	}
+	if err != nil {
+		return fmt.Errorf("can't marshal yaml: \"%#v\"; stack:\"%v\"", valuesResult, err)
+	}
+
+	fmt.Println(string(renderedValues))
+	return nil
+
+}
